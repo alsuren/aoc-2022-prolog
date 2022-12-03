ffb8bab67d36cdff6d5639fce9ecb9d3a68b2de4
David Laban <alsuren@gmail.com>
2022-12-03 22:00:12 +0000

    print input lines enumerated

--- day-1-elf-packs.pl
+++ day-1-elf-packs.pl
@@ -1,4 +1,4 @@
-
+:- use_module(library(error)).
 
 package(empty, elf_1_1, 1000).
 package(elf_1_1, elf_1_2, 2000).
@@ -23,6 +23,25 @@ total(Position, Weight) :-
 
 solution(Max) :- findall(W, total(_, W), Result), max_list(Result, Max).
 
-?- solution(Max), write_ln(Max).
+
+load_arcs(File) :-
+    retractall(arc(_,_)),
+    setup_call_cleanup(
+        open(File, read, Stream),
+        load_arcs_from_stream(1, Stream),
+        close(Stream)).
+
+load_arcs_from_stream(I, Stream) :- !,
+    read_line_to_string(Stream, T0),
+    load_arcs_from_line(I, T0, Stream).
+
+load_arcs_from_line(_, end_of_file, _Stream) :- !.
+load_arcs_from_line(I, Line, Stream) :- !,
+    I1 is I+1,
+    write_ln((I1, Line)),
+    load_arcs_from_stream(I1, Stream).
+
+?- load_arcs("day-1-demo-input").
+% ?- solution(Max), write_ln(Max).
 
 ?- halt.

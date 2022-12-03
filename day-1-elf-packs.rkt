f65f8cfe568e0f457749051a2071abc8142f6786
David Laban <alsuren@gmail.com>
2022-12-03 20:50:43 +0000

    final attempts at reshaping rocket-datalog solution

--- day-1-elf-packs.rkt
+++ day-1-elf-packs.rkt
@@ -6,37 +6,34 @@
 
 % package(Elf, Layer, Weight) - I couldn't find a way to do sum(),
 % so I'm doing it as a series of + operations
-package(1, 11, 1000).
-package(1, 12, 2000).
-package(1, 13, 3000).
+package(1, 1, 2, 1000).
+package(1, 2, 3, 2000).
+package(1, 3, 4, 3000).
 
-package(2, 21, 4000).
+package(2, 1, 2, 4000).
 
-package(3, 31, 5000).
-package(3, 32, 6000).
+package(3, 1, 2, 5000).
+package(3, 2, 3, 6000).
 
-package(4, 41, 7000).
-package(4, 42, 8000).
-package(4, 43, 9000).
+package(4, 1, 2, 7000).
+package(4, 2, 3, 8000).
+package(4, 3, 4, 9000).
 
-package(5, 51, 10000).
+package(5, 1, 2, 10000).
 
-runningtotal(1, 10, 0).
-runningtotal(2, 20, 0).
-runningtotal(3, 30, 0).
-runningtotal(4, 40, 0).
-runningtotal(5, 50, 0).
+runningtotal(1, 0, 0).
+runningtotal(2, 0, 0).
+runningtotal(3, 0, 0).
+runningtotal(4, 0, 0).
+runningtotal(5, 0, 0).
 %runningtotoal(Elf, Layer, Total) :-
 %  Layer :- *(Elf, 10),
 %  package(Elf, Layer, 0).
-runningtotoalxxx(Elf, Layer, Total) :-
-  LayerBelow :- -(Layer, 1),
-  BottomLayer :- *(Elf, 10),
-  Layer != BottomLayer,
-  package(Elf, Layer, T1),
+runningtotoal(Elf, Layer, Total) :-
+  package(Elf, LayerBelow, Layer, T1),
   runningtotoal(Elf, LayerBelow, T2),
   Total :- +(T1, T2).
 
-% runningtotoal(1, 1, Total)?
-runningtotoal(4, 40, Total)?
-% runningtotoal(Elf, Layer, Total)?
+ runningtotoal(1, 1, Total)?
+% runningtotoal(4, 40, Total)?
+%runningtotoal(Elf, Layer, Total)?
